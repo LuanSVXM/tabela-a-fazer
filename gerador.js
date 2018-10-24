05ffb0a350c41ebff0da59ce5bd836af1c9edd96
LuanSVXM <36699408+LuanSVXM@users.noreply.github.com>
2018-10-24 11:41:46 -0300

    Add files via upload

---
+++ gerador.js
@@ -0,0 +1,11 @@
+import {NomeElementos} from './name_elements.js'
+import {NumeroAtomico} from './atomic_element.js'
+import {MassaElementos} from './mass_element.js'
+import {SiglaElemento} from './sigla_element.js'
+export function eu () {
+    console.log(NumeroAtomico[0]);
+    console.log(SiglaElemento[0]);
+    console.log(NomeElementos[0]);
+    console.log(MassaElementos[0]);
+}
+export const test = [NumeroAtomico[0],SiglaElemento[0],NomeElementos[0],MassaElementos[0]]
---
+++ main.js
@@ -0,0 +1,11 @@
+import {eu} from './gerador.js'
+import {test} from './gerador.js'
+document.getElementById("oi").innerHTML+=test[0]
+document.getElementById("oi").innerHTML+='<br>'
+
+document.getElementById("oi").innerHTML+=test[1]
+document.getElementById("oi").innerHTML+=test[2]
+
+
+console.log(document.getElementById("oi"))
+console.log("main.js");
